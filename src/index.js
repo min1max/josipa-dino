3c2aeb65bfa8688b97608b8d1b15e72187e883f3
InOrbit d.o.o <anana.duspara@gmail.com>
2020-02-13 12:42:38 +0100

    final

--- src/index.js
+++ src/index.js
@@ -1,92 +1,201 @@
 //import 'bulma/css/bulma.css';
 //require('bulma/css/bulma.css');
 require('./scss/style.scss'); 
+//require('./scss/style_test.scss'); 
 
 //import Button from '@material-ui/core/Button';
 
-console.log('test')
-
-//import img from './img/map.jpg';
-//console.log(img)
-
+console.log('%c Web development by In0rbit @ planet Earth 🌎', 'background: #222; color: #bada55');
 
 import AOS from 'aos';
 import $ from "jquery";
-//import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
+
+var easyScroll = require('easy-scroll');
+
 AOS.init();
 
 
-
-const second = 1000,
+var second = 1000,
       minute = second * 60,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Jun 27, 2020 00:00:00').getTime(),
+var countDown = new Date('Jun 27, 2020 00:00:00').getTime(),
     x = setInterval(function() {
 
-      let now = new Date().getTime(),
+      var now = new Date().getTime(),
           distance = countDown - now;
 
-        document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+        $('.days').html(Math.floor(distance / (day)));
+        $('.hours').html (Math.floor((distance % (day)) / (hour)));
+        $('.minutes').html (Math.floor((distance % (hour)) / (minute)));
+        $('.seconds').html (Math.floor((distance % (minute)) / second));
       
-      //do something later when date is reached
-      //if (distance < 0) {
+      // do something later when date is reached
+      // if (distance < 0) {
       //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+      //  'IT'S THE DAY!;
+      // }
 
     }, second);
 
-    const potvrdaForma = document.getElementById("potvrda-forma");
-    var potvrda_radios = document.getElementsByName('potvrda');
-    for (let index = 0; index < potvrda_radios.length; index++) {
-      potvrda_radios[index].onclick = function(){
-        console.log(this.value);
-        if(this.value == 'da'){
-          potvrdaForma.classList.add('show-form');
-        }else{
-          potvrdaForma.classList.remove('show-form');
-        }
-      }
-      
-    }
 
 
-const drustvo = document.getElementById("drustvo");
-const drustvoForma = document.getElementById("drustvo-forma");
+var potvrdaRadios = document.getElementsByName('potvrda');
+for (var test = 0; test < potvrdaRadios.length; test++) {
+  potvrdaRadios[test].onclick = function(e){
 
-var radios = document.getElementsByName('drustvo');
-for(var i = 0; i < radios.length; i++){
-    radios[i].onclick = function(){
-      console.log(this.value);
-      if(this.value == 'da'){
-        drustvoForma.classList.add('show-form');
+    if ($("input[name='potvrda']").is(':checked')) {
+      console.log($(e.target).val());
+      if($( e.target ).val() == 'da'){
+        $('.potvrda-forma').addClass('show-form');
       }else{
-        drustvoForma.classList.remove('show-form');
+        $('.potvrda-forma').removeClass('show-form');
+      }
+      var isTestclicked = $('[aria-label="potvrda"]').find('.radbtn').hasClass( "clicked" );
+      //console.log($(e.target).parent().parent());
+      //console.log(isTestclicked);
+      if(isTestclicked){
+        //console.log('IMA KLIKNUTO');
+        $('[aria-label="potvrda"]').find('.radbtn').removeClass( "clicked" );
+        $( e.target ).next().toggleClass('clicked');
+        
+        
+      }else{
+        //console.log( $( e.target ).value);
+        console.log('value: ' + $( e.target ).value);
+        
+        $( e.target ).next().toggleClass('clicked');
       }
     }
+    
+  }
+  
+}
+
+var drustvoRadios = document.getElementsByName('drustvo');
+for (var t = 0; t < drustvoRadios.length; t++) {
+  drustvoRadios[t].onclick = function(e){
+
+    if ($("input[name='drustvo']").is(':checked')) {
+      console.log($(e.target).val());
+      if($( e.target ).val() == 'da'){
+        $('.drustvo-forma').addClass('show-form');
+      }else{
+        $('.drustvo-forma').removeClass('show-form');
+      }
+      var isTestclicked = $('[aria-label="drustvo"]').find('.radbtn').hasClass( "clicked" );
+      //console.log($(e.target).parent().parent());
+      //console.log(isTestclicked);
+      if(isTestclicked){
+        //console.log('IMA KLIKNUTO');
+        $('[aria-label="drustvo"]').find('.radbtn').removeClass( "clicked" );
+        $( e.target ).next().toggleClass('clicked');
+        
+        
+      }else{
+        //console.log( $( e.target ).value);
+        console.log('value: ' + $( e.target ).value);
+        
+        $( e.target ).next().toggleClass('clicked');
+      }
+    }
+    
+  }
+  
 }
 
 
-const add_people_btn = document.getElementById("add_people");
-add_people_btn.addEventListener('click', add_people);
-
-function add_people(event) {
-  let contents = this.parentNode.innerHTML;
+$('.add_people').on("click", function(event) {
+  var contents = this.parentNode.innerHTML;
   var rows = $(this).parent().find('section').length;
-  console.log($(this).parent().find('section').length);
+  var naziv;
+  if($('body').hasClass("hr")){
+  //if($('.hr').is(":visible")){
+    naziv = 'Ime i prezime';
+  } else{
+    naziv = 'Name & Surname';
+  }
+  //console.log($(this).parent().find('section').length);
   var row = '<section class="form-element form-input extraname">';
-  row += '<input type="text" class="form-element-field" name="extraName_'+rows+'" placeholder="Ime i Prezime"/>';
+  row += '<input type="text" class="form-element-field" placeholder=" " name="extraName_'+rows+'"/>';
   row += '<div class="form-element-bar"></div>';
+  row += '<label class="form-element-label">'+ naziv + '</label>';
   row += '</section>';
   row = $(row);
   $(this).before(row);
-  
-}
+});
+
+
+
+
+$('.hrv').on("click", function(e) {
+  console.log('hrv');
+  $("body").css("overflow", "initial");
+  e.preventDefault();
+  //$('.hr').fadeIn(1000);
+  $('#overlay').fadeOut(800);
+});
+
+$('.eng').on("click", function(e) {
+  console.log('eng');
+  e.preventDefault();
+  window.location.href = '/en.html';
+
+  //$('.en').fadeIn(1000);
+  //$('#overlay').fadeOut(800);
+});
+
+
+
+  $('.form-card').on("submit", function(e) {
+    console.log('submit');
+    e.preventDefault();
+
+    
+    $.ajax({
+      type: "POST",
+      url: "form.php",
+      data: $(this).serialize(),
+      success: function(data) {
+        console.log("OUR FORM SUBMITTED CORRECTLY");
+        if(data === 'true'){
+          console.log('data true');
+          $('.form-content').fadeOut();
+          $('.form-info').fadeIn();
+          $('.reply').fadeOut();
+          $(document.body).animate({
+            'scrollTop':   $('#formStart').offset().top
+          }, 2000);
+
+          
+          
+        }else{
+
+          $('.form-error').fadeIn();
+          $('.form-info').fadeOut();
+          console.log('data false');
+          
+        }
+      },
+      error: function(){
+        //dev
+        // $('.form-content').fadeOut();
+        // $('.reply').fadeOut();
+        // $('.form-info').fadeIn(1500);
+        // $(document.body).animate({
+        //   'scrollTop':   $('#formStart').offset().top
+        // }, 2000);
+
+
+
+        //prod
+        // $('.form-error').fadeIn();
+        // $('.form-info').fadeOut();
+      }
+    })
+});
+
+
 
 
